c7eb7c986a2f8ed92a1e32d738a9fbdec2c3abd6
liwenquan <1145821676@qq.com>
2018-05-30 15:41:22 +0800

    init

---
+++ class/SLlabel.h
@@ -0,0 +1,13 @@
+//
+//  SLlabel.h
+//  test11
+//
+//  Created by Mac_UI on 2018/5/30.
+//  Copyright © 2018年 Mac_UI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLlabel : UIView
+-(void)test;
+@end
---
+++ class/SLlabel.m
@@ -0,0 +1,15 @@
+//
+//  SLlabel.m
+//  test11
+//
+//  Created by Mac_UI on 2018/5/30.
+//  Copyright © 2018年 Mac_UI. All rights reserved.
+//
+
+#import "SLlabel.h"
+
+@implementation SLlabel
+-(void)test{
+    NSLog(@"测试");
+}
+@end
